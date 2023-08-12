e3230144560eba45922764c7d9de9497cadae769
Fosa <alexey.fosler@mail.ru>
2023-08-12 11:42:40 +0300

    feat: solved problem

---
+++ leetcode/easy/two_sum.py
@@ -0,0 +1,58 @@
+"""
+https://leetcode.com/problems/two-sum/
+
+1. Two Sum
+Easy
+49.9K
+1.6K
+Companies
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+"""
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for xnd, x in enumerate(nums):
+            for ynd, y in enumerate(nums[xnd + 1:], xnd + 1):
+                if x + y == target:
+                    return [xnd, ynd]
+
+
+if __name__ == "__main__":
+    assert Solution().twoSum(nums=[2, 7, 11, 15], target=9) == [0, 1], Solution().twoSum(nums=[2, 7, 11, 15], target=9)
+    assert Solution().twoSum(nums=[3, 2, 4], target=6) == [1, 2], Solution().twoSum(nums=[3, 2, 4], target=6)
+    assert Solution().twoSum(nums=[3, 3], target=6) == [0, 1], Solution().twoSum(nums=[3, 3], target=6)
+    assert Solution().twoSum(nums=[3, 2, 3], target=6) == [0, 2], Solution().twoSum(nums=[3, 2, 3], target=6)
